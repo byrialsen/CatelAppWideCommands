07b022ce3001b72e84e2f71e5ecaab3a1b7b3d0b
SERVERS\paw.byrialsen <paw.byrialsen@stevanatogroup.com>
2018-11-28 13:57:26 +0100

    Add project files.

---
+++ CatelAppWideCommands.Uwp/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Catel.Windows.Controls;
+
+namespace CatelAppWideCommands.Uwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainView : Page
+    { 
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ CatelAppWideCommands.Uwp/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Catel;
+using Catel.MVVM;
+using Catel.Windows.Input;
+using System;
+using Windows.System;
+
+namespace CatelAppWideCommands.Uwp
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ICommandManager _commandManager;
+
+        public DateTime UpdatedTimestamp { get; set; }
+
+        public Command RefreshCommand { get; set; }
+
+        public MainViewModel(ICommandManager commandManager)
+        {
+            Argument.IsNotNull(() => commandManager);
+
+            _commandManager = commandManager;
+
+            _commandManager.CreateCommand("Refresh", new InputGesture(VirtualKey.F5));
+            RefreshCommand = new Command(() => { UpdatedTimestamp = DateTime.Now; });
+            _commandManager.RegisterCommand("Refresh", RefreshCommand, this);
+        }
+    }
+}
